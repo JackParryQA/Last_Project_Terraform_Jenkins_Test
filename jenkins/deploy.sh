1db836b4507f2428dcf3ccceb09649d361ff8cae
JackParryQA <82821648+JackParryQA@users.noreply.github.com>
2021-07-07 20:03:07 +0100

    Update deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -3,7 +3,7 @@
 #Run Terraform
 cd ./Terraform
 terraform init
-terraform apply \
+terraform apply -auto-approve \
  -var "access_key=$TF_VAR_ACCESS_KEY" \
  -var "secret_key=$TF_VAR_SECRET_KEY" \
  -var "db_password=$TF_VAR_DB_PASSWORD"
