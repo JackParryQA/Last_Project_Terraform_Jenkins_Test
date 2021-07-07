feda79bf98969fe229d7cccc7181fc2995138015
JackParryQA <82821648+JackParryQA@users.noreply.github.com>
2021-07-07 20:32:10 +0100

    update files

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.48.0"
-    }
-  }
-}
-
 provider "aws" {
     access_key = var.access_key
     secret_key = var.secret_key
--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -4,9 +4,10 @@
 sudo apt update -y
 
 #Install Terraform
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=$(dpkg --print-architecture)] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt install terraform
+apt-get update && apt-get install -y gnupg software-properties-common curl
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
+apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+apt-get update && apt-get install terraform
 
 # Install kubectl
 # curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
