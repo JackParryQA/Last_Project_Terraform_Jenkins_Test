5ce7e58d491677c772d6afc8bfb5481144ecc29a
JackParryQA <82821648+JackParryQA@users.noreply.github.com>
2021-07-07 20:45:27 +0100

    Update deploy.sh

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -5,12 +5,12 @@ cd ./Terraform
 
 terraform init
 terraform plan \
- -var "access_key=$TF_VAR_ACCESS_KEY" \
- -var "secret_key=$TF_VAR_SECRET_KEY" \
- -var "db_password=$TF_VAR_DB_PASSWORD"
+ -var="access_key=$TF_VAR_ACCESS_KEY" \
+ -var="secret_key=$TF_VAR_SECRET_KEY" \
+ -var="db_password=$TF_VAR_DB_PASSWORD"
 terraform apply -auto-approve \
- -var "access_key=$TF_VAR_ACCESS_KEY" \
- -var "secret_key=$TF_VAR_SECRET_KEY" \
- -var "db_password=$TF_VAR_DB_PASSWORD"
+ -var="access_key=$TF_VAR_ACCESS_KEY" \
+ -var="secret_key=$TF_VAR_SECRET_KEY" \
+ -var="db_password=$TF_VAR_DB_PASSWORD"
  
  cd ..
